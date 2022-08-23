515d7f7bd1d2386e78ccf8c7c455c417c273e792
AnyaRatusnyak <anyaaboyko@gmail.com>
2022-08-24 00:03:05 +0300

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluide" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluide" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by SheCodes</footer>
+      </div>
     </div>
   );
 }
