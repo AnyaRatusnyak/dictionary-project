dc8faad7dd6b67854a8f48861dce051e4948360d
AnyaRatusnyak <anyaaboyko@gmail.com>
2022-08-28 02:05:58 +0300

    added link to Netlify

--- src/App.js
+++ src/App.js
@@ -13,7 +13,32 @@ export default function App() {
         <main>
           <Dictionary defaultKeyword="hello" />
         </main>
-        <footer className="App-footer">Coded by SheCodes</footer>
+        <footer className="App-footer">
+          This project was coded by{" "}
+          <a
+            href="https://github.com/AnyaRatusnyak"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Anna Ratushnyak
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/AnyaRatusnyak/dictionary-project.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://elegant-cajeta-f48221.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
